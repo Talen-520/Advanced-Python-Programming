8557fda622972915278ce8291127b8593f2b914e
Tao Hu <Tao727188712@gmail.com>
2023-05-07 22:33:03 -0400

    class note added

--- part6.py
+++ part6.py
@@ -143,3 +143,66 @@ sorted_user_letters = "".join(sorted(user_letters.upper()))
 user_word_list = word_dict.get(sorted_user_letters, "no words available")
 
 print(user_word_list)
+
+#use the pickle function to save a list of numbers to a file:
+import pickle
+
+numbers = [1, 2, 3, 4, 5]
+with open("numbers.pkl", "wb") as f:
+ pickle.dump(numbers, f) # dump the object to a file 存储
+ 
+
+with open("numbers.pkl", "rb") as f:
+ restored_numbers = pickle.load(f) # load the object from the file 加载
+print(restored_numbers)
+
+#Here is another example. 
+#We create our Scrabble dictionary and then pickle it for later use. 
+def signature(w):
+    w1=list(w)
+    w1.sort()
+    w1=''.join(w1)
+    return w1
+#create or load?
+mode=input("Create or Load C or L: ")
+print()
+if mode.upper()=='C':
+# create a "Scrabble Dictionary"
+    d={}
+    print('Creating dictionary ... please wait.')
+    f = open("C:/Users/Owner/OneDrive/Desktop/Python Code/Advanced-Python-Programming/text file/wordlist.txt",'r')
+    print()
+    sl = f.read()
+    143
+    z=sl.split(' ')
+    print()
+    for w in z:
+        sig=signature(w)
+        if sig not in d:
+            d[sig]=[]
+            d[sig].append(w)
+        else:
+            d[sig].append(w)
+    f.close()
+else:
+    print('Unpickling dictionary ... please wait.')
+    f=open('slwords','rb')
+    d=pickle.load(f) # this “unpickles”
+word=input("Please enter word: ")
+print()
+while word!='done':
+    if len(word)!=6:
+        print("word not 6 chars")
+    else:
+        word=word.upper()
+        word=signature(word)
+    if word in d:
+        print(d[word])
+    else:
+        print(word,' not found.')
+    word=input("Please enter word: ")
+    print()
+f=open('slwords','wb')
+print('Pickling ... please wait.')
+pickle.dump(d,f) # this pickles
+f.close()
--- temp.py
+++ temp.py
@@ -1,4 +1,48 @@
-import pickle
-numbers = [1, 2, 3, 4, 5]
-with open("numbers.pkl", "wb") as f:
- pickle.dump(numbers, f)
+def signature(w):
+    w1=list(w)
+    w1.sort()
+    w1=''.join(w1)
+    return w1
+#create or load?
+mode=input("Create or Load C or L: ")
+print()
+if mode.upper()=='C':
+# create a "Scrabble Dictionary"
+    d={}
+    print('Creating dictionary ... please wait.')
+    f = open("C:/Users/Owner/OneDrive/Desktop/Python Code/Advanced-Python-Programming/text file/wordlist.txt",'r')
+    print()
+    sl = f.read()
+    143
+    z=sl.split(' ')
+    print()
+    for w in z:
+        sig=signature(w)
+        if sig not in d:
+            d[sig]=[]
+            d[sig].append(w)
+        else:
+            d[sig].append(w)
+    f.close()
+else:
+    print('Unpickling dictionary ... please wait.')
+    f=open('slwords','rb')
+    d=pickle.load(f) # this “unpickles”
+word=input("Please enter word: ")
+print()
+while word!='done':
+    if len(word)!=6:
+        print("word not 6 chars")
+    else:
+        word=word.upper()
+        word=signature(word)
+    if word in d:
+        print(d[word])
+    else:
+        print(word,' not found.')
+    word=input("Please enter word: ")
+    print()
+f=open('slwords','wb')
+print('Pickling ... please wait.')
+pickle.dump(d,f) # this pickles
+f.close()
